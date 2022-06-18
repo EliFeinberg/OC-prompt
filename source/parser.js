760eb91bce6c98766ff67e763f6ea10084767402
Jeffrey Huang <jeffreyhuang@Jeffreys-MacBook-Pro.local>
2022-06-17 17:26:40 -0700

    Parsed files and code for parsing

---
+++ source/parser.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const csv = require("csv");
+
+const result = {};
+const keys = ["\"Text\"", "\"Description\""];
+
+// Read data
+const readStream = fs.createReadStream("commands.txt");
+
+// Parser
+const parser = csv.parse({ delimiter: ":" });
+
+
+parser.on("data", (chunk) => {
+
+var text = chunk[0];
+text = '"'+text+'"';
+result[text] = {};
+    for(let i = 1; i < chunk.length; i ++) {
+        // var tempKey = JSON. stringify(keys[0]);
+        result[text][keys[0]] = chunk[0];
+        result[text][keys[1]] = chunk[1];
+    }
+});
+
+parser.on("end", () => {
+    console.log(result);
+});
+
+// const json = JSON.stringify(result);
+// fs.writeFile('test.json', json, 'utf8')
+
+readStream.pipe(parser);
+
+// 1.run "curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh"
+// 2.run "bash install_nvm.sh"
+// 3. copy end script and run
+// 4. go to file and run "node parser.js"
+// 5. turn all '" to " and turn all "' to "
+// 6. turn all ' to "
