97b1e4a0f4b31f107df8e98818202a984b22d26b
Eli Feinberg <elifeinberg@elis-mbp.usca.ibm.com>
2022-06-21 12:25:24 -0700

    Implemented Command execution

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"go-prompt/libs"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/c-bata/go-prompt"
 )
 
 func completer(d prompt.Document) []prompt.Suggest {
-	text := d.GetWordBeforeCursor()
+	// text := d.GetWordBeforeCursor()
 	// fmt.Println(text)
 	s := libs.ParseFiletoSuggest("source/commands.json")
 
@@ -16,11 +19,29 @@ func completer(d prompt.Document) []prompt.Suggest {
 }
 func main() {
 	fmt.Println("OpenShift Interactive Command Line Interface")
-	t := prompt.Input(
-		"oc ",
-		completer,
-		prompt.OptionTitle("RHOCP CLI"),
-		prompt.OptionSelectedDescriptionTextColor(prompt.DarkGray), prompt.OptionPrefixBackgroundColor(prompt.DarkBlue))
+	for {
+		t := prompt.Input(
+			"oc ",
+			completer,
+			prompt.OptionTitle("RHOCP CLI"),
+			prompt.OptionSelectedDescriptionTextColor(prompt.DarkGray))
+		var ps *exec.Cmd
+		CMDargs := strings.Split(t, " ")
+		if CMDargs[0] == "exit" {
+			os.Exit(0)
+		}
+		if CMDargs[0] == "clear" {
+			ps = exec.Command("clear")
+		} else {
+			ps = exec.Command("oc", CMDargs...)
+		}
 
-	fmt.Println("You selected: oc", t)
+		res, err := ps.Output()
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(string(res))
+		}
+	}
 }
