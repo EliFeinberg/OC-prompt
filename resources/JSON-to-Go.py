0cf64f86fd2a2ae4f14810d6cdb1120949784399
Eli Feinberg <elifeinberg@elis-mbp.usca.ibm.com>
2022-07-07 12:00:33 -0700

    Added in usage info for each command

--- resources/JSON-to-Go.py
+++ resources/JSON-to-Go.py
@@ -1,17 +1,20 @@
 import json
 import os
 file_list = os.listdir('../source')
+print("{")
 for files in file_list:
     if '.json' in files and files != "commands.json":
         f = open("../source/{}".format(files))
         data = json.load(f)
-        print("\"{}\":".format(files[:len(files)-5])+ "{")
-        for points in data:
-            k = data[points]["Text"]
-            v = data[points]["Description"]
-            print("\tprompt.Suggest{"+"Text: \"{}\", Description: \"{}\"".format(k,v)+"},")
-        print("},")
-        f.close()
+        print("\t"+ "\"{}\":".format(files[:len(files)-5])+"\"{}\"".format(data["Usage"])+",")
+        # print("\"{}\":".format(files[:len(files)-5])+ "{")
+        # for points in data:
+        #     k = data[points]["Text"]
+        #     v = data[points]["Description"]
+        #     print("\tprompt.Suggest{"+"Text: \"{}\", Description: \"{}\"".format(k,v)+"},")
+        # print("},")
+        # f.close()
+print("}")
         
 
 # f = open("../source/commands.json", "r")
